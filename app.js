bffa304adbaabe8f2c508c13e22e812db53a4a36
sg1973_dev <sg1973@gmail.com>
2020-12-03 07:48:42 -0500

    modified

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWS Code Pipeline Hello!'));
+app.get('/', (req, res) => res.send('AWS Beanstalk command line Hello!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
